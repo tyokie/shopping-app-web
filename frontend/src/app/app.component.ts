186dd1335ee280b3ea07d6b5861bd99000c58dea
Tawanda Kamuzonde <me@tawanda.kamuzonde.com>
2017-04-27 03:45:48 +1000

    * Added Initial UI layout & hooked into data from payload.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import { ProductService } from './product.service';
+import { MdCard, MdMenu, MdButton } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+
+  products: any[];
+  totalProducts: number;
+  totalCost: number;
+
+  constructor (private _productService: ProductService) { }
+
+  ngOnInit () {
+    this._productService.getProductsSummary().subscribe(( products ) => {
+      console.log(products);
+      this.products = products;
+    });
+  }
+
+  calculatePackCost(item): number
+  {
+    return item.unitsInCartons * item.unitCost;
+  }
+
+  displayPackRecommendations(items):boolean
+  {
+    return items.length > 1;
+  }
+
+  calculateRecommendationPercentage(recommendation): string
+  {
+    let calc = Math.round((recommendation.currentLevel * recommendation.maxLevel) / 100);
+    return `${calc}%`;
+  }
+
 }
